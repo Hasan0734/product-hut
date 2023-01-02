4277840e73bd1f74e4223d7a3b96ca49fff994a5
Jahid Hasan <jahidhasan2856@gmail.com>
2023-01-02 21:33:23 +0600

    fix  the spacing on latest blog

--- components/Cards/LatestBlogCard.tsx
+++ components/Cards/LatestBlogCard.tsx
@@ -8,7 +8,7 @@ const LatestBlogCard = ({blog}:any) => {
                 <img className='rounded-lg w-full  h-[255px] lg:h-[200px]' src={blog.image} alt="Shoes" />
                 <div className='p-3 py-5'>
                     <div className='flex mb-5 gap-x-7'>
-                        <p className='josefin-font text-sm font-semibold items-center flex gap-x-2 text-[#949495]'>
+                        <p className='josefin-font text-sm font-semibold items-center flex gap-x-2 text-[#151875]'>
                             <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M8.02667 0L11.3333 3.31333L10.38 4.25333L7.08 0.953333L8.02667 0ZM0 10.8533L4.33333 6.54C4.26667 6.33333 4.31333 6.07333 4.48667 5.9C4.74667 5.64 5.17333 5.64 5.43333 5.9C5.69333 6.16667 5.69333 6.58667 5.43333 6.84667C5.26 7.02 5 7.06667 4.79333 7L0.48 11.3333L7.55333 8.96667L9.90667 4.72667L6.61333 1.42667L2.36667 3.78L0 10.8533Z" fill="#FB2E86" />
                             </svg>
--- components/LatestBlog/LatestBlog.tsx
+++ components/LatestBlog/LatestBlog.tsx
@@ -4,17 +4,19 @@ import LatestBlogCard from '../Cards/LatestBlogCard';
 
 const LatestBlog = () => {
     return (
-        <div className='container px-4 py-5'>
-            <h2 className='text-center text-[28px] font-bold  leading-[32px] text-[#151875] josefin-font'>Latest Blog</h2>
-            <div className='mt-12'>
-                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10'>
-                    
-                    <LatestBlogCard blog={{ image: "https://placeimg.com/400/229/arch"}} />
-                    <LatestBlogCard blog={{ image: "https://placeimg.com/400/226/arch"}} />
-                    <LatestBlogCard blog={{ image: "https://placeimg.com/400/227/arch"}} />
+        <div className='py-20'>
+            <div className='container px-4'>
+                <h2 className='text-center text-[28px] font-bold  leading-[32px] text-[#151875] josefin-font'>Latest Blog</h2>
+                <div className='mt-12'>
+                    <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10'>
+
+                        <LatestBlogCard blog={{ image: "https://placeimg.com/400/229/arch" }} />
+                        <LatestBlogCard blog={{ image: "https://placeimg.com/400/226/arch" }} />
+                        <LatestBlogCard blog={{ image: "https://placeimg.com/400/227/arch" }} />
+                    </div>
                 </div>
             </div>
-        </div>
+       </div>
     );
 };
 
